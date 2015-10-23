87f085ab609316629742812ed7dd4c4cfb9f3bd1
Franck LAPEYRE <fnk.lap@gmail.com>
2015-10-23 16:44:54 +0200

    edit composer and test

---
+++ src/AppBundle/Tests/Controller/ApplicationControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+
+    }
+
+    public function testIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
