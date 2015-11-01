9f9ad1b568ca4b0f663545038d95482458414d92
Franck LAPEYRE <fnk.lap@gmail.com>
2015-11-02 00:05:30 +0100

    add nelmioApiDocBundle

---
+++ src/AppBundle/Controller/CardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class CardController extends Controller
+{
+
+}
